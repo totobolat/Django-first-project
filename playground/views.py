c44eb44cebe4deb87bdf4b80ad7939208d0f7b79
totobolat <tbolat06@gmail.com>
2024-01-12 23:10:22 +0300

    query

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
 from store.models import Product
+from store.models import OrderItem
 
 
 def calculate():
@@ -11,8 +13,6 @@ def calculate():
 
 
 def say_hello(request):
-    try:
-        queryset = Product.objects.filter(unit_price__gt=20)
-    except ObjectDoesNotExist:
-        return render(request, 'hello.html', {'name': 'Tolga'})
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values_list('product_id').distinct()).order_by('title')
     return render(request, 'hello.html', {'name': 'Tolga', 'products': list(queryset)})
+#OrderItem.objects.values('product_id')
