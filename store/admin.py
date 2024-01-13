bf695fe4839aa792ce5954d18d538cecba2f9669
totobolat <tbolat06@gmail.com>
2024-01-13 21:39:30 +0300

    admin

--- store/admin.py
+++ store/admin.py
@@ -4,6 +4,20 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
+    list_editable = ['unit_price']
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory <10:
+            return 'LOW'
+        return 'OK'
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name','last_name']
+
 
 admin.site.register(models.Collection)
